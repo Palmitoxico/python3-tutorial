843c2f09475e0c9b7d78d2de6da490027e5ca985
Augusto Fraga Giachero <augustofg96@gmail.com>
2017-07-12 11:26:09 -0300

    Input/Output Example

---
+++ io.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+
+#
+# Input/Output example
+#
+# Sum two numbers read from stdin and print the answer
+#
+
+a = int(input("Write a number: "))
+b = int(input("Write another number: "))
+print("Sum of the two numbers: " + str(a + b))
